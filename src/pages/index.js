7e99f1d2b5c5e41b072dee45e07ca54942dbe325
mikeQ95 <krismorrow1982@gmail.com>
2026-05-18 18:55:42 +0800

    换成ahri作为头像

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ function HomepageHeader() {
         </div>
         <div className={styles.heroImageContainer}>
           <img 
-            src={useBaseUrl('/img/颗秘！.jpg')} 
+            src={useBaseUrl('/img/ahri.png')} 
             alt="Profile Avatar" 
             className={styles.heroImage}
           />
