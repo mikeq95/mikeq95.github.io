b522bf9d08fe40f32793d7c001dcb4604a74573a
mikeq95 <mikeq95@users.noreply.github.com>
2026-07-05 06:50:17 +0000

    deploy: df4471235b377b1dac957cca7c7c748b9d2da8f2

---
+++ en/assets/js/22f9ffc3.2a06dd62.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_blog=self.webpackChunkmy_blog||[]).push([["8915"],{6004(e){e.exports=JSON.parse('{"metadata":{"permalink":"/en/blog","page":1,"postsPerPage":10,"totalPages":2,"totalCount":20,"nextPage":"/en/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
